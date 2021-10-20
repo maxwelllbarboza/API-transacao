383c15d8a7a25ee99b12333ccee6e4d41dead536
maxwell Barboza <maxwellbarboza@hotmail.com>
2021-10-19 21:36:08 -0300

    Configuração da conexão do banco de dados

--- src/conexao.js
+++ src/conexao.js
@@ -1,14 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'unxvhtyvjjkmpo',
-    host: 'ec2-52-201-72-91.compute-1.amazonaws.com',
-    database: 'dddfrq2qjiv4hh',
-    password: 'cd3821c47476567109bce2d4f4ace1ec142aa3cd4b8a4fcf443f398733b76796',
-    port: 5432,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    user: 'postgres',
+    host: 'localhost',
+    database: 'market_cubos',
+    password: '123456',
+    port: 5432
 });
 
 
