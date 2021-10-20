db2b2429e4a9a5871619e847efe616e3b0a190c2
maxwell Barboza <maxwellbarboza@hotmail.com>
2021-10-20 15:58:23 -0300

    Configurar Banco de Dados

--- src/conexao.js
+++ src/conexao.js
@@ -1,5 +1,4 @@
 const { Pool } = require('pg');
-
 const pool = new Pool({
     user: 'database-transacao',
     host: 'app-424c9947-4aee-4f1d-9faa-f333c73f3af6-do-user-10072902-0.b.db.ondigitalocean.com',
@@ -7,7 +6,6 @@ const pool = new Pool({
     password: 'W5gTPpglQVD0RnQd',
     port: 25060
 });
-
 const query = (text, param) => {
     return pool.query(text, param);
 }
