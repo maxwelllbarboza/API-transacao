67f755cc036288635f0349240a186a96f5d6f4a8
maxwell Barboza <maxwellbarboza@hotmail.com>
2021-10-20 01:03:06 -0300

    configuracao dos dados do banco de dadoss

--- src/conexao.js
+++ src/conexao.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'database-transacao',
-    host: 'app-2878e29b-f1ab-49c9-a7ec-a1d9e4845d73-do-user-10072902-0.b.db.ondigitalocean.com',
-    database: 'database-transacao',
-    password: 'fl7bnVcg1ytumyzv',
-    port: 25060
+    user: 'postgres',
+    host: 'localhost',
+    database: 'market_cubos',
+    password: '123456',
+    port: 5432
 });
 
 
