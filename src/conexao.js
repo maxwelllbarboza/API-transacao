424ae6ea51f21236011504da508a30fb5aa6f6f2
maxwell Barboza <maxwellbarboza@hotmail.com>
2021-10-20 23:12:29 -0300

    Congiguração do banco de dados

--- src/conexao.js
+++ src/conexao.js
@@ -4,7 +4,11 @@ const pool = new Pool({
     host: 'app-424c9947-4aee-4f1d-9faa-f333c73f3af6-do-user-10072902-0.b.db.ondigitalocean.com',
     database: 'database-transacao',
     password: 'W5gTPpglQVD0RnQd',
-    port: 25060
+    port: 25060,
+    ssl:{
+        rejectUnauthorized: false
+    }
+
 });
 const query = (text, param) => {
     return pool.query(text, param);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { eventNames } = require('npmlog');
 const app = require('./servidor');
 app.listen(3000);
 
